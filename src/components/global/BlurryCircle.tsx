3d67524803a68282651b14586969602bea690542
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2025-01-25 23:15:53 +0200

    Fixing Blurry Circle Direction.

--- src/components/global/BlurryCircle.tsx
+++ src/components/global/BlurryCircle.tsx
@@ -3,6 +3,6 @@ type propsType = {
 };
 
 function BlurryCircle({ direction = 'left' }: propsType) {
-  return <div className={`bg-[#A7FFBC] absolute -z-10 -${direction}-1/4 top-[100px] w-1/2 h-[300px] rounded-full blur-[775px]`}></div>;
+  return <div className={`bg-[#A7FFBC] absolute -z-10 ${direction === 'left' ? '-left-1/4' : '-right-1/4 '} top-[100px] w-1/2 h-[300px] rounded-full blur-[775px]`}></div>;
 }
 export default BlurryCircle;
