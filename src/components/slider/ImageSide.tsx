62df1ed02d4026ea5c808ad1ee8ed48bef5f2146
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2025-01-26 22:05:43 +0200

    Updating Slider Image Size.

--- src/components/slider/ImageSide.tsx
+++ src/components/slider/ImageSide.tsx
@@ -11,7 +11,7 @@ function ImageSide() {
 
   return (
     <div className="relative mx-auto max-w-[500px">
-      <div className="absolute top-10 max-w-[350px] grid grid-cols-[auto,1fr,auto] items-center gap-4 text-main bg-light-green p-3 md:p-4 rounded-xl">
+      <div className="relative md:absolute max-md:right-2 top-10 max-w-[320px] grid grid-cols-[auto,1fr,auto] items-center gap-4 text-main bg-light-green p-3 md:p-4 rounded-xl">
         <img src={correctIcon} alt="Icon" />
         <div className="grid gap-1">
           <h5 className="font-bold">{data.title}</h5>
@@ -23,12 +23,12 @@ function ImageSide() {
         </div>
       </div>
 
-      <div className="w-[90%] max-w-[450px] mx-auto rounded-[40px] overflow-hidden bg-white">
+      <div className="w-full md:w-[90%] max-w-[450px] mx-auto rounded-[40px] overflow-hidden bg-white">
         <img src={`/mothalath-agency/imgs/slider/hero/hero-${id}.png`} alt="Photo" className="w-full max-h-[500px] object-cover" />
       </div>
 
-      <div className="grid grid-cols-[1fr,auto] gap-12 relative pt-4 pr-4 -mt-[100px] mr-auto w-fit bg-[#79FD730D] rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10">
-        <img src={triangle} alt="Triangle" className="justify-self-start w-14" />
+      <div className="grid grid-cols-[40px,140px] md:grid-cols-[1fr,auto] gap-8 md:gap-12 relative pt-4 pr-4 -mt-[100px] mr-auto w-fit bg-[#79FD730D] rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10">
+        <img src={triangle} alt="Triangle" className="justify-self-start w-14 max-w-full" />
         <img src={`/mothalath-agency/imgs/slider/illustrations/illustration-${id}.png`} alt="Photo" className="justify-self-end" />
       </div>
     </div>
