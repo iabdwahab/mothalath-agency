38dc86d69e95a5750470abd40ae8ae3c3af5e8dd
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2025-01-26 08:53:38 +0200

    Fixing Navbar Design Issue.

--- src/components/header/data.tsx
+++ src/components/header/data.tsx
@@ -9,7 +9,7 @@ export const navLinksElements = navLinks.map((navLink, index) => {
   return (
     <>
       <li key={navLink.id} className="max-md:w-full">
-        <a href={navLink.link} className={`hover:text-supporter duration-medium font-bold block max-md:py-5 max-md:w-full max-md:text-center ${index !== 0 ? 'border-t' : ''}`}>
+        <a href={navLink.link} className={`hover:text-supporter duration-medium font-bold block max-md:py-5 max-md:w-full max-md:text-center ${index !== 0 ? 'max-md:border-t' : ''}`}>
           {navLink.text}
         </a>
       </li>
