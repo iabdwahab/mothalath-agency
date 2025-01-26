f18397b45758ac8c9dbe865387efabf91567353c
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2025-01-26 08:23:12 +0200

    Edit Mobile Navbar Design.

--- src/components/header/data.tsx
+++ src/components/header/data.tsx
@@ -5,12 +5,14 @@ export const navLinks = [
   { id: 4, text: 'المدونة', link: '#' },
 ];
 
-export const navLinksElements = navLinks.map((navLink) => {
+export const navLinksElements = navLinks.map((navLink, index) => {
   return (
-    <li key={navLink.id} className="max-md:w-full">
-      <a href={navLink.link} className="hover:text-supporter duration-medium font-bold block max-md:py-2 max-md:w-full max-md:text-center">
-        {navLink.text}
-      </a>
-    </li>
+    <>
+      <li key={navLink.id} className="max-md:w-full">
+        <a href={navLink.link} className={`hover:text-supporter duration-medium font-bold block max-md:py-5 max-md:w-full max-md:text-center ${index !== 0 ? 'border-t' : ''}`}>
+          {navLink.text}
+        </a>
+      </li>
+    </>
   );
 });
