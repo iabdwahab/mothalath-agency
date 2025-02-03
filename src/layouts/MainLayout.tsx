bfb1284d3f5feaf0bea37a07c9b4aef47456f431
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2025-02-03 02:36:47 +0200

    Scroll To Top when pathname changing.

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,11 +1,19 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Articles from "../components/articles/Index";
 import Footer from "../components/footer/Index";
 import Header from "../components/header/Index";
 import Questions from "../components/questions/Index";
 import Testimonials from "../components/testimonials/Index";
+import { useEffect } from "react";
 
 function MainLayout() {
+  const location = useLocation();
+  const pathname = location.pathname;
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <div className="relative overflow-x-hidden bg-contain bg-top text-white">
       <Header />
